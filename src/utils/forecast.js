0857ae9c918e9f51e58734def302b3f52fb3a046
safe-halabe <safehalabe@gmail.com>
2020-05-21 18:50:29 +0300

    Add wind speed to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,26 @@
-const request = require("request");
+const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
-  const accessKey = "1f2831048b919f83e474d2d161df01ef";
+  const accessKey = '1f2831048b919f83e474d2d161df01ef';
   const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${longitude},${latitude}&units=m`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to weather service!", undefined);
+      callback('Unable to connect to weather service!', undefined);
     } else if (body.error) {
-      callback("Unable to find location", undefined);
+      callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
-        "It is " +
+        'It is ' +
           body.current.weather_descriptions[0] +
-          ". the temprtior is " +
+          '. the temprtior is ' +
           body.current.temperature +
-          " degress. " +
-          " it feelslike " +
+          ' degress. ' +
+          ' it feelslike ' +
           body.current.feelslike +
-          " degress."
+          ' degress and the wind speed is : ' +
+          body.current.wind_speed
       );
     }
   });
