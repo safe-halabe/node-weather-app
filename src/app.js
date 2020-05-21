dab98834a988a1ccf6147fe289baac3d87ada9cb
safe-halabe <safehalabe@gmail.com>
2020-05-21 18:02:36 +0300

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -1,51 +1,52 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewPath);
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
 hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-    name: "Safe Halabe",
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Safe Halabe',
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About Me",
-    name: "Safe Halabe",
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Safe Halabe',
   });
 });
 
-app.get("/help", (req, res) => {
-  res.render("help", {
-    title: "Help page",
-    name: "Safe Halabe",
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page',
+    name: 'Safe Halabe',
     msg:
-      "  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis esse non animi porro cupiditate enim magni molestias totam provident ullam?",
+      '  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis esse non animi porro cupiditate enim magni molestias totam provident ullam?',
   });
 });
 
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "You must provide an address!",
+      error: 'You must provide an address!',
     });
   }
 
@@ -71,10 +72,10 @@ app.get("/weather", (req, res) => {
   );
 });
 
-app.get("/products", (req, res) => {
+app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: "You must provide a search term",
+      error: 'You must provide a search term',
     });
   }
 
@@ -84,22 +85,22 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Safe halabe",
-    errorMessage: "Help article not found.",
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Safe halabe',
+    errorMessage: 'Help article not found.',
   });
 });
 
-app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Safe halabe",
-    errorMessage: "Page not found.",
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Safe halabe',
+    errorMessage: 'Page not found.',
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
 });
